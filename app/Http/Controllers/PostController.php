52189b2abcabce7eb075ad5fa016d7f4bea596f3
zira <alnaziraa241@gmail.com>
2025-01-20 20:53:48 +0700

    haduh

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->paginate(5);
-        return view('posts.index,', compact('posts'));
+        return view('posts.index', compact('posts'));
     }
 
     /**
