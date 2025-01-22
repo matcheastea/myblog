7659a65c485d5e9ec4f7c0e5336f52fd0e0e58ef
zira <alnaziraa241@gmail.com>
2025-01-22 15:07:43 +0700

    selesai

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,10 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="{{ route('posts.index') }}">
+                                        {{ __('Post') }}
+                                    </a>
+                                    
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
